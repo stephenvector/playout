6da57cd058f257ef3e377e4a7fd2fb8dd8a13e0f
Stephen Vector <stephenvector@pm.me>
2019-10-27 23:17:36 -0400

    Renaming files.

--- src/components/controls/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as FieldGroupField } from "./FieldGroupField";
-export { default as SelectField } from "./SelectField";
-export { default as TextField } from "./TextField";
-export { default as TextareaField } from "./TextareaField";
