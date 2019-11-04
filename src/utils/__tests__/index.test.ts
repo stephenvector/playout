ca8fb5d2bc2f3223f650b55c762149b379fbb3cb
Stephen Vector <stephenvector@pm.me>
2019-11-04 16:48:52 -0500

    Fixing tests.

--- src/utils/__tests__/index.test.ts
+++ src/utils/__tests__/index.test.ts
@@ -1,22 +1,10 @@
 import "@testing-library/jest-dom/extend-expect";
-import { getRandomID, getYupSchemaFromContentTypeFields } from "../";
+import { getRandomID } from "../";
 
 describe("getRandomID()", () => {
   it("Generates random string of given length.", () => {
     const t = getRandomID([], 8, "a");
 
     expect(t.length).toEqual(8);
-  });
-});
-
-describe("getYupSchemaFromContentTypeFields()", () => {
-  it("Should create valid schema types", () => {
-    const schema = getYupSchemaFromContentTypeFields([
-      {
-        id: "title",
-        type: "text",
-        name: "Title"
-      }
-    ]);
   });
 });
